1af0cd660c8aae690dcab4c48fdfd4ba419a59a2
Andy Wong <andy.wongtszkiu@gmail.com>
2024-09-25 02:32:54 +0800

    asd

--- controllers/auth.js
+++ controllers/auth.js
@@ -132,10 +132,9 @@ export const login = async (req, res) => {
 
     res
       .cookie("access_token", token, {
-        httpOnly: true, // Prevent access via JavaScript
+        httpOnly: false, // Prevent access via JavaScript
         secure: process.env.NODE_ENV === "production", // Only set Secure in production
         sameSite: "None", // Allow cross-site cookies
-        path: "/",
       })
       .status(200)
       .json({
