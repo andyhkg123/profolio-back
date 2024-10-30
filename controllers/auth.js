d98070aa9a4834bc0c90406389ac40294c85f0d4
Andy Wong <andy.wongtszkiu@gmail.com>
2024-10-31 07:34:57 +0800

    asd

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,22 +2,41 @@ import { client } from "../db.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-const secret = process.env.JWT_SECRET;
+// export const register = (req, res) => {
+//   const { fullname, email, password } = req.body;
 
-export function generateToken(payload) {
-  // console.log("JWT_SECRET during generation:", process.env.JWT_SECRET);
-  if (!secret) {
-    throw new Error("JWT secret is not defined");
-  }
-  return jwt.sign(payload, secret, { expiresIn: "1h" });
-}
+//   if (!fullname || !email || !password) {
+//     return res
+//       .status(400)
+//       .json({ error: "All fields (username, email, password) are required" });
+//   }
 
-export function verifyToken(token) {
-  if (!secret) {
-    throw new Error("JWT secret is not defined");
-  }
-  return jwt.verify(token, secret);
-}
+//   const checkUserQuery =
+//     "SELECT * FROM profolio.users WHERE email = ? OR password = ?";
+
+//   db.query(checkUserQuery, [email, password], (err, data) => {
+//     if (err) {
+//       console.error("Database query error:", err);
+//       return res.status(500).json({ error: "Internal server error" });
+//     }
+//     if (data.length) {
+//       return res.status(409).json({ error: "User email already existed" });
+//     }
+
+//     var salt = bcrypt.genSaltSync(10);
+//     var hash = bcrypt.hashSync(password, salt);
+
+//     const insertUser =
+//       "INSERT INTO profolio.users (fullname,email , password) VALUES (?,?,?)";
+//     db.query(insertUser, [fullname, email, hash], (err, data) => {
+//       if (err) {
+//         console.error("Database query error:", err);
+//         return res.status(500).json({ error: "Internal server error" });
+//       }
+//       return res.status(201).json({ message: "user has been created" });
+//     });
+//   });
+// };
 
 export const register = async (req, res) => {
   const { fullname, email, password } = req.body;
@@ -55,51 +74,87 @@ export const register = async (req, res) => {
   }
 };
 
+// export const login = (req, res) => {
+//   const { email, password } = req.body;
+
+//   const checkUserLogin = "SELECT * FROM profolio.users WHERE email = ?";
+
+//   db.query(checkUserLogin, [email], (err, data) => {
+//     if (err) {
+//       console.error("Database query error:", err);
+//       return res.status(500).json({ error: "Internal server error" });
+//     }
+//     console.log(req.body.password);
+
+//     if (!data.length) {
+//       return res.status(404).json({ message: "user not found" });
+//     }
+
+//     if (data.length) {
+//       const checkPassword = bcrypt.compareSync(password, data[0].password);
+//       if (!checkPassword) {
+//         return res.status(401).json({ error: "incorrect password" });
+//       } else {
+//         const token = jwt.sign({ id: data[0].email }, "jwtkey");
+//         const { password, ...other } = data[0];
+//         res
+//           .cookie("access_token", token)
+//           .status(200)
+//           .json({
+//             ...other,
+//             message: "User logged in",
+//           });
+//       }
+//     }
+//   });
+// };
+
 export const login = async (req, res) => {
-  if (req.method === "POST") {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    try {
-      const db = await connectToDatabase();
-      const users = db.collection("users");
-      const user = await users.findOne({ email });
+  try {
+    await client.connect();
+    const database = client.db("profolio");
+    const users = database.collection("users");
 
-      if (!user) {
-        return res.status(404).json({ message: "User not found" });
-      }
-
-      const isPasswordValid = bcrypt.compareSync(password, user.password);
-      if (!isPasswordValid) {
-        return res.status(401).json({ error: "Incorrect password" });
-      }
-
-      const token = generateToken({ id: user.email }, "jwtkey");
-      res.setHeader(
-        "Set-Cookie",
-        `access_token=${token}; HttpOnly; Path=/; SameSite=Strict; Max-Age=3600`
-      );
-
-      return res.status(200).json({ message: "User logged in", user });
-    } catch (err) {
-      console.error("Database query error:", err);
-      return res.status(500).json({ error: "Internal server error" });
+    const user = await users.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-  } else {
-    res.setHeader("Allow", ["POST"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+
+    const isPasswordValid = bcrypt.compareSync(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: "Incorrect password" });
+    }
+
+    const token = jwt.sign({ id: user.email }, "jwtkey");
+    const { password: userPassword, ...other } = user;
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: false, // Prevent access via JavaScript
+        secure: true, // Only set Secure in production
+        sameSite: "None", // Allow cross-site cookies
+      })
+      .status(200)
+      .json({
+        ...other,
+        message: "User logged in",
+      });
+  } catch (err) {
+    console.error("Database query error:", err);
+    return res.status(500).json({ error: "Internal server error" });
+  } finally {
+    await client.close();
   }
 };
 
 export const logout = (req, res) => {
-  if (req.method === "POST") {
-    res.setHeader(
-      "Set-Cookie",
-      `access_token=; HttpOnly; expires=Thu, 01 Jan 1970 00:00:00 GMT;`
-    );
-
-    res.status(200).json({ message: "User has been logged out" });
-  } else {
-    res.setHeader("Allow", ["POST"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
+  res
+    .clearCookie("access_token", {
+      sameSite: "none",
+      secure: true,
+    })
+    .status(200)
+    .json("User has been logged out");
 };
