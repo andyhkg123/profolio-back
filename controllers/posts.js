88bdb67abdbdd74b356aca8572ccc89ceed9d956
Andy Wong <andy.wongtszkiu@gmail.com>
2024-11-01 07:06:44 +0800

    as

--- controllers/posts.js
+++ controllers/posts.js
@@ -12,7 +12,6 @@ export const getPosts = async (req, res) => {
     const database = client.db("profolio");
     const blog = database.collection("blog");
     const posts = await blog.find({}).toArray();
-    console.log(posts);
     return res.status(200).json(posts);
   } catch (err) {
     return res.status(500).json(err); // Handle the error appropriately
@@ -24,17 +23,12 @@ export const getPosts = async (req, res) => {
 export const addPost = async (req, res) => {
   const { fullname_blog, title, content } = req.body;
 
-  console.log("Request headers:", req.headers);
-  console.log("Cookies:", req.cookies);
-
   const token = req.cookies.access_token;
 
   if (!token) return res.status(401).json("Not authenticated!");
 
   jwt.verify(token, process.env.JWT_SECRET, async (err, userInfo) => {
     if (err) return res.status(403).json("Token is not valid!");
-
-    console.log(userInfo.id);
 
     try {
       await client.connect();
@@ -48,8 +42,6 @@ export const addPost = async (req, res) => {
         fullname_blog,
       };
       const result = await blog.insertOne(newPost);
-
-      console.log(result);
 
       return res.json("Post has been created.");
     } catch (err) {
